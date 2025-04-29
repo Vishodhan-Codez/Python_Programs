38c6aa18ebf0e025b3755f7ae058c59058aeab48
Vishodhan-Codez <vishodhan.ks@gmail.com>
2025-04-29 17:33:42 +0530

    H.W. fix

--- L-43 Time and Space Compelxity/H.W. Multiply By N.py
+++ L-43 Time and Space Compelxity/H.W. Multiply By N.py
@@ -4,7 +4,7 @@ def method1(a,b) : # for 1 iteration
     return a*b
 def method2(a,b) : # for n interations
     pro = 0
-    for _ in range(n) :
-        pro += m
+    for _ in range(b) :
+        pro += a
     return pro
 print(f"The product of {m} and {n} by\nMethod 1 is {method1(m,n)}\nMethod 2 is {method2(m,n)}")
