da89e4ba0ccfaef27d0ccabd01210f825da60968
Vishodhan-Codez <vishodhan.ks@gmail.com>
2025-07-03 17:55:49 +0530

    Update 1

---
+++ L-55 Recursion/H.W. Recursion Time.py
@@ -0,0 +1,12 @@
+# Time Complexity Analysis
+
+# Function: fac(n)
+# This function calculates factorial of n .
+# Each call subtracting n by 1 and performs one multiplication.
+# Total calls = n
+# => Time Complexity: n
+
+# Function: print1to10(n)
+# This function prints numbers from n to 10 recursively.
+# At most 10 calls, each doing a simple print.
+# => Time Complexity: 10
---
+++ M-1 Python Basics/More on Python/nonrepeatingelementinarray.py
@@ -0,0 +1,10 @@
+arr = [0,2,4,6,8,0,2,4,8]
+res = 0
+# output - 6 
+for i in arr :
+    res = res^i
+print(res)
+"""
+111
+000
+"""
